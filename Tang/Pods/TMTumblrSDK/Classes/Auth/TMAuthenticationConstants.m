1a631484fe9460e55d29836784164a7ae7b02b49
wangbo <developforapple@163.com>
2018-01-22 17:32:05 +0800

    update

--- Tang/Pods/TMTumblrSDK/Classes/Auth/TMAuthenticationConstants.m
+++
@@ -1,9 +0,0 @@
-//
-//  TMAuthenticationConstants.m
-//  Pods
-//
-//  Created by Tyler Tape on 4/17/17.
-//
-//
-
-NSString * _Nonnull const AuthorizationHeaderField = @"Authorization";
--- Tang/Pods/TMTumblrSDK/Classes/Response Parsing/TMHTTPResponseErrorCodes.m
+++
@@ -1,9 +0,0 @@
-//
-//  TMHTTPResponseErrorCodes.m
-//  Pods
-//
-//  Created by Kenny Ackerson on 4/27/16.
-//
-//
-
-NSInteger TMHTTPResponseErrorCodeWrongResponseType = 44;
