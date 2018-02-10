a072544096bd427682541717d74503620c785782
tiny <developforapple@163.com>
2018-02-10 13:58:28 +0800

    update

---
+++ Tang/Tang/AppCode/General/NetworkAvailability.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkAvailability.swift
+//  Tang
+//
+//  Created by wwwbbat on 2018/2/10.
+//  Copyright © 2018年 Tiny. All rights reserved.
+//
+
+import UIKit
+
+class NetworkAvailability: NSObject {
+
+}
